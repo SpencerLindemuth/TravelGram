ab5efd03bb8e3ff96a85cb1ae8667a32a3302be9
Hao Wang <ge_nah@hotmail.com>
2019-07-09 11:28:52 -0700

    update session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
     else
-      flash[:danger] = 'Invalid login info, please try to login again or create'
+      flash[:danger] = 'Invalid login info, please try to login again or create a new account'
       render 'new'
     end
   end
