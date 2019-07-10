3b5778ffb560f58cadde9285f63ec4c3db6c11aa
Spencer Lindemuth <lindemut@gmail.com>
2019-07-10 11:40:11 -0700

    create and link city:show and location:show

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,8 @@
 class CitiesController < ApplicationController
+    def show
+        @city = City.find(params[:id])
+        @city_locations = @city.locations
+        @city_posts = @city_locations.map{ |location| location.posts}.flatten
+
+    end
 end
